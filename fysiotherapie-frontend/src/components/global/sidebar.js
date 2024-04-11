f945f97250a11395cf73c8164448391daaf487e7
Thuutje <thuutje2@gmail.com>
2024-04-11 13:22:01 +0200

    fixed sidebar, padding deleted in sidebar

--- fysiotherapie-frontend/src/components/global/sidebar.js
+++ fysiotherapie-frontend/src/components/global/sidebar.js
@@ -4,11 +4,17 @@ class SidebarComponent extends LitElement {
     static get styles() {
         return css`
           .sidebar {
-            height: 94.3vh; 
+            height: 100vh; 
             background-color: #3297DF;
             color: white;
-            padding: 20px;
             overflow-y: hidden;
+            text-align: center;
+          }
+
+          .line {
+            width: 100%; 
+            height: 2px; 
+            background-color: white; 
           }
 
         `;
@@ -19,6 +25,7 @@ class SidebarComponent extends LitElement {
             <div class="sidebar">
                 <img src="./images/RunningMan.png" alt="Logo" width="100" height="100">
                 <h1>Dashboard</h1>
+                <div class="line"></div>
             </div>
         `;
     }
