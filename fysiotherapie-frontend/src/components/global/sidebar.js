aeee729e624735b29c7558d483862f6fa639ca1b
Thuutje <thuutje2@gmail.com>
2024-04-17 10:48:23 +0200

    Added new links and dropdown
    New links + dropdown for Fysiotherapeut

--- fysiotherapie-frontend/src/components/global/sidebar.js
+++ fysiotherapie-frontend/src/components/global/sidebar.js
@@ -2,6 +2,7 @@ import {css, html, LitElement} from "lit";
 
 class SidebarComponent extends LitElement {
     static get styles() {
+
         return css`
           :host {
             font-family: Inter, sans-serif;
@@ -16,7 +17,7 @@ class SidebarComponent extends LitElement {
             position: relative; /* Hiermee kunnen we de position: absolute van de #logout-btn relatief aan de .sidebar maken */
           }
 
-          .sidebar img {
+          .sidebar .image-sidebar {
             margin-top: 20px;
           }
 
@@ -35,8 +36,7 @@ class SidebarComponent extends LitElement {
             text-decoration: none;
             cursor: pointer;
           }
-
-
+          
           .line {
             width: 100%; 
             height: 2px; 
@@ -48,12 +48,35 @@ class SidebarComponent extends LitElement {
             padding: 10px 0;
             color: white;
             border-bottom: 1px solid white;
+            text-decoration: none;
           }
 
           .links-container a.active {
             background-color: #1E5B9E;
           }
 
+          .dropdown-menu {
+            list-style-type: none;
+            text-decoration: none;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            background-color: #3297DF; 
+            padding: 0; 
+            margin: 0;
+          }
+
+          .dropdown-menu li a {
+            display: block;
+            padding: 10px; 
+            color: white;
+            text-decoration: none;
+          }
+
+          .dropdown-image {
+            margin-left: 5px;
+          }
+          
           #logout-btn {
             position: absolute;
             bottom: 10px; 
@@ -67,7 +90,13 @@ class SidebarComponent extends LitElement {
             height: 25px;
           }
 
+
         `;
+    }
+
+    constructor() {
+        super();
+        this.isDropdownVisible = false;
     }
 
     render() {
@@ -83,6 +112,15 @@ class SidebarComponent extends LitElement {
                     <!-- en dan hier moet iets komen van als je patient bent dan zie je deze links-->
                     <a href="/patient-information" @click="${this.handleLinkClick}">Mijn gegevens</a>
                     <a href="/patient-history" @click="${this.handleLinkClick}">Historie</a>
+                    <!--FYSIOTHERAPEUT-->
+                    <!-- en dan hier moet iets komen van als je fysiotherapeut bent dan zie je deze links-->
+                    <a href="/patient-overview" @click="${this.handleLinkClick}">Patiënten</a>
+                    <a @click="${this.toggleDropdown}">Activiteit <img src="../../../public/sort-down.png" width="15" height="15" class="dropdown-image"> </a>
+                    <ul class="dropdown-menu" ?hidden="${!this.isDropdownVisible}">
+                        <li><a href="/#" @click="${this.handleLinkClick}">Lopen</a></li>
+                        <li><a href="/#" @click="${this.handleLinkClick}">Gooien</a></li>
+                        <li><a href="/#" @click="${this.handleLinkClick}">Rennen</a></li>
+                    </ul>
                 </div>
 
                 <a id="logout-btn" href="#" @click="${this.handleLogout}">
@@ -97,6 +135,11 @@ class SidebarComponent extends LitElement {
         const links = this.shadowRoot.querySelectorAll('.links-container a');
         links.forEach(link => link.classList.remove('active'));
         event.target.classList.add('active');
+    }
+
+    toggleDropdown() {
+        this.isDropdownVisible = !this.isDropdownVisible;
+        this.requestUpdate();
     }
 }
 
