fb93aeabad662fa58a8ffcc618c2030caaf5b34d
Fleurhoogeveen122 <Fleurhoogeveen122@gmail.com>
2024-05-13 16:10:40 +0200

    Grafieken vergelijken obv checkbox werkt

--- fysiotherapie-frontend/src/components/activity-walk.js
+++ fysiotherapie-frontend/src/components/activity-walk.js
@@ -9,9 +9,19 @@ class ActivityWalk extends LitElement {
                 font-family: Inter, sans-serif;
             }
 
+            .header {
+                display: flex;
+                align-items: center;
+                height: 20px;
+            }
+
             .title {
                 font-size: 36px;
                 margin-top: auto;
+            }
+
+            input[type="file"] {
+                margin-left: auto;
             }
 
             .line {
@@ -28,12 +38,21 @@ class ActivityWalk extends LitElement {
 
             .chart-row {
                 display: flex;
-                flex-wrap: wrap;
-                justify-content: space-between;
-                flex-basis: calc(33.33% - 10px);
+                flex-direction: column;
+                align-items: center;
                 margin-bottom: 20px;
             }
 
+            .chart-row > div {
+                margin-bottom: 10px; 
+                font-size: 14px; 
+            }
+
+            .chart-row canvas {
+                width: 100%;
+                height: auto;
+            }
+            
             .select-data {
                 margin-bottom: 10px;
             }
@@ -75,8 +94,19 @@ class ActivityWalk extends LitElement {
         super();
         this.loadChartLibrary().then(() => {
             this.createCharts();
+            this.filterCharts();
         });
-        this.selectedOptions = new Set();
+        this.checkedValues = {
+            'right-knee': false,
+            'left-knee': false,
+            'left-hip': false
+        };
+        this.handleAllCheckboxChange = this.handleAllCheckboxChange.bind(this);
+    }
+
+    async firstUpdated() {
+        await this.loadChartLibrary();
+        this.createCharts();
     }
 
     async loadChartLibrary() {
@@ -93,31 +123,49 @@ class ActivityWalk extends LitElement {
     createCharts() {
         const xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
         const chartData = [
-            { data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
+            { data: [10, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
             { data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000], borderColor: "black", fill: false },
             { data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100], borderColor: "black", fill: false },
 
-            { data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
+            { data: [2000, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
             { data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000], borderColor: "black", fill: false },
             { data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100], borderColor: "black", fill: false },
 
-            { data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
+            { data: [5000, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478], borderColor: "black", backgroundColor: "black", fill: false },
             { data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000], borderColor: "black", fill: false },
             { data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100], borderColor: "black", fill: false }
         ];
 
         const chartContainer = this.shadowRoot.querySelector('.chart-container');
-        let currentRow;
 
         chartData.forEach((data, index) => {
-            if (index % 3 === 0) {
-                currentRow = document.createElement('div');
-                currentRow.classList.add('chart-row');
-                chartContainer.appendChild(currentRow);
+            const canvas = document.createElement('canvas');
+            const chartRow = document.createElement('div');
+            chartRow.classList.add('chart-row');
+            const chartWrapper = document.createElement('div');
+            chartWrapper.classList.add('chart-wrapper');
+
+            const indexTexts = {
+                0: "Left-knee x-data",
+                1: "Left-knee y-data",
+                2: "Left-knee z-data",
+                3: "Right-knee x-data",
+                4: "Right-knee y-data",
+                5: "Right-knee z-data",
+                6: "Left-hip x-data",
+                7: "Left-hip y-data",
+                8: "Left-hip z-data",
+            };
+            if (indexTexts.hasOwnProperty(index)) {
+                const textElement = document.createElement('div');
+                textElement.textContent = indexTexts[index];
+                chartRow.appendChild(textElement);
             }
 
-            const canvas = document.createElement('canvas');
-            currentRow.appendChild(canvas);
+            chartRow.appendChild(canvas);
+            chartWrapper.appendChild(chartRow);
+            chartContainer.appendChild(chartWrapper);
+
             new window.Chart(canvas, {
                 type: "line",
                 data: {
@@ -129,7 +177,10 @@ class ActivityWalk extends LitElement {
                 }
             });
         });
+        this.filterCharts();
     }
+
+
 
 
     toggleCollapse() {
@@ -140,35 +191,47 @@ class ActivityWalk extends LitElement {
 
     handleCheckboxChange(event) {
         const { value, checked } = event.target;
-        const checkboxes = this.shadowRoot.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = checkbox.value === value && checked;
+        this.checkedValues[value] = checked;
+        this.filterCharts();
+    }
+
+
+    filterCharts() {
+        const chartRows = this.shadowRoot.querySelectorAll('.chart-row');
+        const visibleRows = Array.from(chartRows).filter(row => row.style.display !== 'none');
+        const flexBasis = visibleRows.length > 0 ? `calc(${100 / visibleRows.length}% - 10px)` : '100%';
+        chartRows.forEach(row => {
+            row.style.flexBasis = flexBasis;
+        });
+
+        chartRows.forEach((row, index) => {
+            const groupName = this.getGroupName(index);
+            const shouldDisplay = this.checkedValues[groupName];
+            row.style.display = shouldDisplay ? 'flex' : 'none';
+        });
+    }
+
+
+    getGroupName(index) {
+        const groupIndex = Math.floor(index / 3);
+        if (groupIndex === 0) {
+            return 'left-knee';
+        } else if (groupIndex === 1) {
+            return 'right-knee';
+        } else if (groupIndex === 2) {
+            return 'left-hip';
+        }
+    }
+
+    handleAllCheckboxChange(event) {
+        const { checked } = event.target;
+        Object.keys(this.checkedValues).forEach(key => {
+            this.checkedValues[key] = checked;
         });
         this.filterCharts();
     }
 
-    filterCharts() {
-        const chartRows = this.shadowRoot.querySelectorAll('.chart-row');
-        chartRows.forEach(row => {
-            const groupName = row.dataset.group; // Groepnaam van de rij
-            const shouldDisplay = Array.from(row.querySelectorAll('input[type="checkbox"]')).some(checkbox => checkbox.checked && checkbox.value === groupName);
-            row.style.display = shouldDisplay ? 'flex' : 'none'; // Toon de rij als een van de geselecteerde checkboxes overeenkomt met de groep
-        });
-    }
 
-
-
-
-    getGroupName(index) {
-        const groupIndex = Math.floor(index / 3); // Bereken de index van de groep (0 voor right-knee, 1 voor left-knee, 2 voor left-hip)
-        if (groupIndex === 0) {
-            return 'right-knee'; // Groepnaam voor de eerste drie grafieken
-        } else if (groupIndex === 1) {
-            return 'left-knee'; // Groepnaam voor de middelste drie grafieken
-        } else {
-            return 'left-hip'; // Groepnaam voor de laatste drie grafieken
-        }
-    }
 
 
     render() {
@@ -180,16 +243,20 @@ class ActivityWalk extends LitElement {
                 <label for="select-data" class="collapse-btn" @click="${this.toggleCollapse}">Selecteer datatype:</label>
                 <div class="select-box" id="select-box">
                     <div class="option">
+                        <input type="checkbox" id="optionAll" value="all" @change="${this.handleAllCheckboxChange}">
+                        <label for="optionAll">All data</label>
+                    </div>
+                    <div class="option">
                         <input type="checkbox" id="option1" value="right-knee" @change="${this.handleCheckboxChange}">
-                        <label for="option1">right-knee</label>
+                        <label for="option1">Right-knee</label>
                     </div>
                     <div class="option">
                         <input type="checkbox" id="option2" value="left-knee" @change="${this.handleCheckboxChange}">
-                        <label for="option2">left-knee</label>
+                        <label for="option2">Left-knee</label>
                     </div>
                     <div class="option">
                         <input type="checkbox" id="option3" value="left-hip" @change="${this.handleCheckboxChange}">
-                        <label for="option3">left-hip</label>
+                        <label for="option3">Left-hip</label>
                     </div>
                 </div>
             </div>
