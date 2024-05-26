1ffa49ff99c71bef48b6a3aa5e5e3fa15b3f461c
MaritaMe <114008523+MaritaMe@users.noreply.github.com>
2024-05-26 13:53:43 +0200

    JWT token to half an hour

--- fysiotherapie-backend/src/main/java/fysiotherapie/security/CorsConfig.java
+++ fysiotherapie-backend/src/main/java/fysiotherapie/security/CorsConfig.java
@@ -2,8 +2,6 @@ package fysiotherapie.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
