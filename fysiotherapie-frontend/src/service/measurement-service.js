7ed6f40e4afaaae128795242e35374c7f4913870
MaritaMe <114008523+MaritaMe@users.noreply.github.com>
2024-05-31 15:04:40 +0200

    Added getMeasurement method in measurement-service.js

--- fysiotherapie-frontend/src/service/measurement-service.js
+++ fysiotherapie-frontend/src/service/measurement-service.js
@@ -14,4 +14,20 @@ export default class MeasurementService {
             return {success: false, error: errorText || "Uploaden van bestand mislukt. Probeer opnieuw"}
         }
     }
+
+    static async getMeasurement(patientId, treatmentId, measurementId) {
+        const fetchOptions = {
+            method: "GET",
+            headers: {"Authorization": "Bearer " + sessionStorage.getItem("myToken")},
+        }
+        const response = await fetch(
+            `http://localhost:8080/patients/${patientId}/treatments/${treatmentId}/measurements/${measurementId}`,
+            fetchOptions)
+        if (response.ok) {
+            return {success: true, measurement: response.json()}
+        } else {
+            const errorText = await response.text();
+            return {success: false, error: errorText || "Het ophalen is mislukt"}
+        }
+    }
 }
