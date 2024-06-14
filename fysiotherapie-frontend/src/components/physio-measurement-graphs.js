2e72db2cd036e76a51207f10a5bc25f85400d5c8
MaritaMe <114008523+MaritaMe@users.noreply.github.com>
2024-06-14 21:51:54 +0200

    Patients of physio own page with treatments and measurement information

--- fysiotherapie-frontend/src/components/physio-measurement-graphs.js
+++ fysiotherapie-frontend/src/components/physio-measurement-graphs.js
@@ -8,22 +8,28 @@ class PhysioMeasurementGraphs extends LitElement {
             patientId: { type: String },
             treatmentId: { type: String },
             measurementId: { type: String },
-            measurement: { type: Object }
+            measurement: { type: Object },
+            jointTypes: { type: Array },
+            checkedValues: { type: Object }
         };
     }
 
     constructor() {
         super();
         this.measurement = null;
+        this.checkedValues = {};
     }
 
     async connectedCallback() {
+        debugger;
         super.connectedCallback();
         this.patientId = this.location.params.patientId;
         this.treatmentId = this.location.params.treatmentId;
         this.measurementId = this.location.params.measurementId;
 
+        this.requestUpdate();
         this.measurement = await this.loadMeasurement(this.patientId, this.treatmentId, this.measurementId);
+        this.jointTypes = this.measurement.map(jointData => jointData.jointType);
     }
 
     async loadMeasurement(patientId, treatmentId, measurementId) {
@@ -34,6 +40,11 @@ class PhysioMeasurementGraphs extends LitElement {
         return null;
     }
 
+    handleCheckboxChange(event) {
+        const { id, checked } = event.target;
+        this.checkedValues = { ...this.checkedValues, [id]: checked };
+    }
+
     static get styles() {
         return css`
             :host {
@@ -41,13 +52,55 @@ class PhysioMeasurementGraphs extends LitElement {
                 padding: 1em;
                 position: relative; 
             }
+
+            .checkboxContainer {
+                border: 1px solid #ccc; 
+                padding: 10px; 
+            }
+
+            .checkbox {
+                margin-right: 10px;
+            }
+            
+            .loader {
+                width: 20px;
+                aspect-ratio: 4;
+                background: radial-gradient(circle closest-side,#000 90%,#0000) 0/calc(100%/3) 100% space;
+                clip-path: inset(0 100% 0 0);
+                animation: l1 1s steps(4) infinite;
+            }
+            @keyframes l1 {to{clip-path: inset(0 -34% 0 0)}}
         `;
     }
 
     render() {
+        if (!this.measurement) {
+            return html`
+                <div class="container">
+                    <h2>Meting ${this.measurementId}</h2>
+                    <div class="loader"></div>
+                </div>
+            `;
+        }
+
         return html`
             <div class="container">
                 <h2>Meting ${this.measurementId}</h2>
+                <div class="checkboxContainer">
+                    ${this.jointTypes.map(jointType => html`
+                        <input
+                                class="checkbox"
+                                type="checkbox"
+                                id="${jointType}"
+                                name="${jointType}"
+                                value="${jointType}"
+                                @change="${this.handleCheckboxChange}"
+                                ?checked="${this.checkedValues[jointType]}"
+                        >
+                        <label for="${jointType}">${jointType}</label><br>
+                    `)}
+                </div>
+            </div>
         `;
     }
 
