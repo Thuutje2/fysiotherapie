e75d701669f74ced519cf37d32281edf2e515715
MaritaMe <114008523+MaritaMe@users.noreply.github.com>
2024-06-29 12:32:51 +0200

    main.bat back to what it was

--- fysiotherapie-frontend/batch/main.bat
+++ fysiotherapie-frontend/batch/main.bat
@@ -1,7 +1,7 @@
 @echo off
-cd C:\Users\marit\Documents\GitHub\fysiotherapie\fysiotherapie-frontend\uploads
+cd C:\Users\daanf\code\TODSS\Github\fysiotherapie\fysiotherapie-frontend\uploads
 
-call C:\Users\marit\miniconda3\Scripts\activate.bat
+call C:\Users\daanf\anaconda3\Scripts\activate.bat
 
 @REM call conda clean --all -y
 call conda create -n Sports2D python=3.10 -y
@@ -9,3 +9,4 @@ call conda activate Sports2D
 call pip install sports2d
 
 ipython -c "from Sports2D import Sports2D; Sports2D.detect_pose('Config.toml'); Sports2D.compute_angles('Config.toml')"
+
